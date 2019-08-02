c0ca9783f3a023e07ecf8a3f80e95d24ef808eda
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-03 02:13:59 +0300

    Basic navbar implementation

--- e-commerce-app/src/components/Navbar.js
+++ e-commerce-app/src/components/Navbar.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+
 
 export default class Navbar extends Component {
     render() {
         return (
             <div>
-                <h3>Hello from Navbar</h3>
+                <nav className="navbar navbar-expand-md bg-success navbar-dark px-sm-5">
+                    {/* https://www.iconfinder.com/icons/185072/book_read_streamline_icon */}
+                    <Link to="/">
+                        <img src={logo} alt="Logo" className="navbar-brand"/>
+                    </Link>
+                    <ul className="navbar-nav align-items-center">
+                        <li className="nav-item ml-5">
+                            <Link to="/" className="nav-link" style={{color: '#eee'}}>
+                                Products
+                            </Link>
+                        </li>
+                    </ul>
+                    <Link to="/cart" className="ml-auto" style={{color: '#ddd'}}>
+                        <i className="fas fa-shopping-cart" />
+                        my cart
+                    </Link>
+                </nav>
             </div>
         )
     }
