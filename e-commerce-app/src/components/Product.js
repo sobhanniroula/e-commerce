52e09e6a7748a4ee97fce7576da2944c911aa33d
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-03 23:03:10 +0300

    card layout and cart-btn implementation

--- e-commerce-app/src/components/Product.js
+++ e-commerce-app/src/components/Product.js
@@ -32,6 +32,10 @@ export default class Product extends Component {
                                 }
                         </button>
                     </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="text-green4 align-self-center mb-0">{title}</p>
+                        <h6 className="mb-0"><span className="mr-1">$</span>{price}</h6>
+                    </div>
                 </div>
 
 
@@ -42,5 +46,51 @@ export default class Product extends Component {
 
 
 const ProductWrapper = styled.div`
+    .card {
+        border-color: transparent;
+        transition: all 1s linear;
+    }
 
+    .card-footer {
+        background: transparent;
+        transition: all 1s linear;
+    }
+    &:hover {
+        .card {
+            border: 0.04rem solid rgba(0,0,0,0.2);
+            box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+        }
+        .card-footer {
+            background: rgba(247,247,247);
+        }
+    }
+    .card-img-top {
+        transition: all 1s linear;
+    }
+    .img-container {
+        position: relative;
+        overflow: hidden;
+    }
+    .img-container:hover .card-img-top {
+        transform: scale(1.1);
+    }
+    .cart-btn {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        padding: 0.5rem 1rem;
+        margin-bottom: 0.1rem;
+        background: #009977e8;
+        border: none;
+        color: #eee;
+        border-radius: 0.5rem 0 0 0;
+        font-size: 1.1rem;
+        transition: all 0.5s linear;
+    }
+
+    .cart-btn:hover {
+        font-size: 1.2rem;
+        background: #0099aae8;
+        cursor: pointer;
+    }
 `;
