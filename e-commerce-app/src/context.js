bb964fd863f1c89aa8910356e93f3da1dc867947
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-04 22:58:00 +0300

    Unit testing for data.js

--- e-commerce-app/src/context.js
+++ e-commerce-app/src/context.js
@@ -24,7 +24,20 @@ class ProductProvider extends Component {
         console.log('Hello from addToCart');
     }
 
-    
+    tester = () => {
+        console.log('State products: ', this.state.products[0].inCart);
+        console.log('Data products: ', storeProducts[0].inCart);
+
+        const tempProducts = [...this.state.products];
+        tempProducts[0].inCart = true;
+        this.setState(() => {
+            return {products: tempProducts}
+        },() => {
+            console.log('State products: ', this.state.products[0].inCart);
+            console.log('Data products: ', storeProducts[0].inCart);
+        })
+    }
+
     render() {
         return (
             <ProductContext.Provider value={{
@@ -32,7 +45,7 @@ class ProductProvider extends Component {
                     handleDetail: this.handleDetail,
                     addToCart: this.addToCart
                 }}>
-                   
+                    <button onClick={this.tester}>Test Me</button>
                 {this.props.children}
             </ProductContext.Provider>
         )
